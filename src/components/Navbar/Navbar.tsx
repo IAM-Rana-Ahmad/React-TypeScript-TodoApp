e2620d66b887bf3f0d6fa743da1bb39230e6e74f
IAM-Rana-Ahmad <r80228927@gmail.com>
2023-12-30 18:53:22 +0000

    first commit

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,46 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar.css";
+interface NavProps{
+    toogle:Boolean,
+    handleToogle:()=>void;
+}
 
-const Navbar:React.FC = () => {
+const Navbar: React.FC<NavProps> = ({toogle, handleToogle}) => {
+  
+  
+    const navLinks=[
+        {name:"Home"},
+        {name:"About"},
+        {name:"Contact"},
+        {name:"Blog"}
+    ]
     return (
-        <div className="container">
-            <nav className="navbar navbar-expand-lg  p-3">
+        <div className="container-fluid" style={{
+            backgroundColor:toogle? "#080B2F":"orange"
+        }} >
+            <nav className="navbar container navbar-expand-lg  p-3">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <a className={`navbar-brand ${toogle?"text-light":"text-dark"}`} href="/">Navbar</a>
+                    <button className="navbar-toggler bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        <ul className={`navbar-nav me-auto mb-2 mb-lg-0 `}>
+                           {navLinks?.map((item,i)=>{
+                            return(
+                                <li className="nav-item mx-3 mx-sm-0" key={i}>
+                                <a className={`nav-link active ${toogle?"text-light":"text-dark"}`} aria-current="page" href="/">{item.name}</a>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="/">Action</a></li>
-                                    <li><a className="dropdown-item" href="/">Another action</a></li>
-                                    <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="/">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href='/' aria-disabled="true">Disabled</a>
-                            </li>
+                            )
+                           })}
+                          
                         </ul>
+
+
                         <div>
                             <label className="switch-button" >
                                 <div className="switch-outer">
-                                    <input id="switch" type="checkbox"/>
-                                        <div className="button">
-                                            <span className="button-toggle"></span>
-                                            <span className="button-indicator"></span>
-                                        </div>
+                                    <input onClick={handleToogle} id="switch" type="checkbox" />
+                                    <div className="button">
+                                        <span className="button-toggle"></span>
+                                        <span className="button-indicator"></span>
+                                    </div>
                                 </div>
                             </label>
                         </div>
--- src/components/todo/todo.tsx
+++ src/components/todo/todo.tsx
@@ -1,5 +1,7 @@
 import React, { FormEvent, useState } from 'react';
 import Navbar from '../Navbar/Navbar';
+import './todo.css'
+
 
 interface TodoProps {
   todo: string;
@@ -9,20 +11,30 @@ interface TodoProps {
 interface TodoItem {
   id: number;
   task: string;
-  marked: boolean; 
-  length:number;
+  marked: boolean;
+  length: number;
 }
 
 const Todo: React.FC<TodoProps> = ({ todo, setTodo }) => {
   const [addTodo, setAddTodo] = useState<TodoItem[]>([]);
   const [idCounter, setIdCounter] = useState(1);
+  const [search, setSearch] = useState("")
+  const [subHeading, setSubHeading] = useState<string>()
+  const [toogle, setToogle] = useState(false)
+
+
+  const handleToogle = () => {
+    setToogle(!toogle)
+  }
+
 
   const addTask = (e: FormEvent) => {
     e.preventDefault();
     if (!todo) {
       alert("Please enter something");
     } else {
-      const newTodo: TodoItem = { id: idCounter, length:todo.length, task: todo, marked: false };
+      const newTodo: TodoItem = { id: idCounter, length: todo.length, task: todo, marked: false };
+      localStorage.setItem("text", todo)
       setAddTodo([...addTodo, newTodo]);
       setTodo('');
       setIdCounter(idCounter + 1);
@@ -63,58 +75,102 @@ const Todo: React.FC<TodoProps> = ({ todo, setTodo }) => {
     });
     setAddTodo(updatedTasks);
   };
+  const handleSearch = (e: FormEvent) => {
+    e.preventDefault();
+    addTodo && addTodo.length > 0
+      ? setAddTodo(
+        addTodo.filter((item) =>
+          item.task.toLowerCase().includes(search.toLowerCase())
+        )
+      )
+
+      : alert("Enter something to find first");
+  };
+
+  const handleSubHeading = () => {
+    const string = prompt("enter a sub heading")
+    if (string) {
+      setSubHeading(string)
+    } else {
+      alert("Enter sub heading first")
+    }
+  }
+
+
+
 
   return (
     <>
-    <Navbar/>
-    <div className="container">
-      <div className="row d-flex justify-content-center mt-5">
-        <h2 className="text-info text-center display-3 fw-bold mt-5">MY TODO APP</h2>
-        <div className="col-xxl-8 mt-5">
-          <div className="input-group mb-3">
-            <input
-              type="text"
-              className="form-control p-3 fs-4"
-              placeholder="Enter Your Work"
-              value={todo}
-              onChange={(e) => setTodo(e.target.value)}
-              aria-label="Recipient's username"
-              aria-describedby="button-addon2"
-            />
-            <button className="btn btn-outline-secondary" type="submit" onClick={addTask} id="button-addon2">
-              Add Task
-            </button>
-          </div>
-        </div>
-      </div>
+      <Navbar toogle={toogle} handleToogle={handleToogle} />
 
-      <div className="row d-flex justify-content-center">
-        {addTodo.map((item) => (
-          <div className="col-xxl-8" key={item.id}>
-            <div className="card w-100 mb-3 shadow-lg">
-              <div className="card-body">
-                <h5 className={item.marked ? "card-header text-decoration-line-through" : "card-header"}>{item.task}</h5>
-                <p>Total Number of Words:{item.length}</p>
-                <div className="d-flex justify-content-between">
-                  <button className="btn btn-danger" onClick={() => handleDelete(item.id)}>
-                    Delete
-                  </button>
-                  <button className="btn btn-primary" onClick={() => handleMark(item.id)}>
-                    {item.marked ? "Unmark" : "Mark"}
-                  </button>
-                  <button className='btn btn-success' onClick={()=>handleUpperCase(item.id)}>
-                    To UpperCase
-                  </button>
-                  <button className='btn btn-info' onClick={()=>handleLowerCase(item.id)}>
-                    To LowerCase
-                  </button>
-                </div>
+      <div className={`container-fluid py-5 h-100 ${toogle? "bg-dark":"bg-warning"}`}>
+        <div className='container '>
+          <form className="d-flex" role="search">
+            <input onChange={(e) => setSearch(e.target.value)} className="form-control me-2 p-3" type="search" placeholder="Enter your task name.." aria-label="Search" />
+            <button onClick={handleSearch} className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+
+
+
+
+
+        <div className="container">
+          <div className="row d-flex justify-content-center mt-5">
+            <h2 className="text-info text-center display-3 fw-bold mt-5">MY TODO APP</h2>
+            <div className="col-xxl-8 mt-5">
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control p-3 fs-4"
+                  placeholder="Enter Your Work"
+                  value={todo}
+                  onChange={(e) => setTodo(e.target.value)}
+                  aria-label="Recipient's username"
+                  aria-describedby="button-addon2"
+                />
+                <button className="btn btn-outline-secondary" type="submit" onClick={addTask} id="button-addon2">
+                  Add Task
+                </button>
               </div>
             </div>
           </div>
-        ))}
+
+          <div className="row d-flex justify-content-center">
+            {addTodo.map((item) => (
+              <div className="col-xxl-8" key={item.id}>
+                <div className="card w-100 mb-3 shadow-lg">
+                  <div className="card-body">
+                    <h5 className={item.marked ? "card-header text-decoration-line-through" : "card-header"}>{item.task}</h5>
+
+                    <span>
+
+                      <p>{subHeading}</p>
+
+                      <button onClick={handleSubHeading} className='btn btn-sm btn-outline-primary my-3'>add sub heading</button>
+                    </span>
+                    <p>Total Number of Words:{item.length}</p>
+                    <div className="d-flex justify-content-between flex-wrap  ">
+                      <button className="btn btn-danger" onClick={() => handleDelete(item.id)}>
+                        Delete
+                      </button>
+                      <button className="btn btn-primary" onClick={() => handleMark(item.id)}>
+                        {item.marked ? "Unmark" : "Mark"}
+                      </button>
+                      <button className='btn btn-success' onClick={() => handleUpperCase(item.id)}>
+                        To UpperCase
+                      </button>
+                      <button className='btn btn-info' onClick={() => handleLowerCase(item.id)}>
+                        To LowerCase
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
     </>
   );
 };
