3f7acbc0082e74d3acdefecfc26b9ade40bf8306
IAM-Rana-Ahmad <r80228927@gmail.com>
2023-12-31 21:11:03 +0000

    adding comments to the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
+// import the todo component from the todo
 import Todo from './components/todo/todo';
 
 function App() {
+  // Here i can define the satate variable todo for hold the value of todo
   const [todo, setTodo] = useState<string>(''); 
   return (
     <div className="App">
+
+      {/* Here i can use the todo component and pass the todo state variable and setTodo function to it */}
       <Todo todo={todo} setTodo={setTodo} /> 
     </div>
   );
